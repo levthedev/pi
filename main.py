29e6aa39a353ef513187dabf748b774e4eebebae
Lev Kravinsky <kravinskylev@gmail.com>
2017-04-20 17:06:52 -0400

    tts

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-try:
-    import Image
-except ImportError:
-    from PIL import Image
+# import espeak
 import pytesseract
-print(pytesseract.image_to_string(Image.open('small_type.jpg')))
+import pyttsx
+from PIL import Image
+
+text = pytesseract.image_to_string(Image.open('img/distorted.jpg'))
+engine = pyttsx.init()
+engine.say(text)
+engine.runAndWait()
+# es = espeak.ESpeak
+# es.say(text)
