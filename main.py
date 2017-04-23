0b26c084e3d456d3f87c48c3e87383c3f0db86d1
Lev Kravinsky <kravinskylev@gmail.com>
2017-04-23 14:13:31 -0400

    update main.py for picamera

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-# import espeak
+import io
 import pytesseract
 import pyttsx
+import pycamera
+from time import sleep
 from PIL import Image
 
-text = pytesseract.image_to_string(Image.open('img/distorted.jpg'))
+stream = io.BytesIO()
+with picamera.PiCamera() as camera:
+    camera.start_preview()
+    sleep(5)
+    camera.capture(stream, format="jpeg")
+
+print("captured photo")
+stream.seek(0)
+image = Image.open(stream)
+text = pytesseract.image_to_string(image)
+text = text.replace("\n", ", ").replace("\r", "").encode("ascii", "ignore")
+print("converted text: {}".format(text))
 engine = pyttsx.init()
+print("created engine")
 engine.say(text)
+print("added text to queue")
 engine.runAndWait()
-# es = espeak.ESpeak
-# es.say(text)
+print("said text")
